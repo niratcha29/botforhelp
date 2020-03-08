f14ba367c15587cefe9d31befd5c96eb9367b14d
niratchajit <babyicezy@gmail.com>
2020-03-08 15:01:51 +0700

    no message

--- src/main/java/com/iphayao/LineApplication.java
+++ src/main/java/com/iphayao/LineApplication.java
@@ -1,23 +1,23 @@
 package com.iphayao;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import org.apache.xmlbeans.impl.xb.ltgfmt.TestCase.Files;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import io.swagger.models.Path;
 
-@SpringBootApplication(scanBasePackages = { "com.iphayao.linebot"
-		,"com.iphayao.repository"
-		,"com.iphayao.service"
-		,"com.iphayao.linenotify"})
+@SpringBootApplication(scanBasePackages = { "com.pico.communication.controller"
+		,"com.pico.communication.service"
+		,"com.pico.communication.config"
+		,"com.pico.communication.dao"})
 public class LineApplication extends SpringBootServletInitializer {
 	public static Path downloadedContentDir;
     public static void main(String[] args) throws IOException {
-//        downloadedContentDir = Files.createTempDirectory("line-bot");
+        downloadedContentDir = Files.createTempDirectory("line-bot");
         SpringApplication.run(LineApplication.class, args);
     }
 
