581d24939261fcc927018268b0524e47b2a7d551
niratchajit <babyicezy@gmail.com>
2020-03-08 14:40:50 +0700

    no message

--- src/main/java/com/iphayao/linebot/Application.java
+++ src/main/java/com/iphayao/linebot/Application.java
@@ -1,23 +1,24 @@
-//package com.iphayao.linebot;
-//
-//
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//import com.iphayao.LineApplication;
-//
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//@SpringBootApplication
-//public class Application {
-//
-//  public static Path downloadedContentDir;
-//	
-//	public static void main(String[] args) throws IOException {
-//	downloadedContentDir = Files.createTempDirectory("line-bot");
-//	SpringApplication.run(LineApplication.class, args);
-//	
-//	}
-//}
+package com.iphayao.linebot;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.google.common.io.Files;
+
+import io.swagger.models.Path;
+
+public class Application extends SpringBootServletInitializer {
+	public static Path downloadedContentDir;
+    public static void main(String[] args) throws IOException {
+//        downloadedContentDir = Files.createTempDirectory("line-bot");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
