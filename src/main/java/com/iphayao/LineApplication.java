732376a6a00d2d5f667c793d393b3e99517c9060
niratchajit <babyicezy@gmail.com>
2020-03-08 14:59:26 +0700

    no message

--- src/main/java/com/iphayao/LineApplication.java
+++ src/main/java/com/iphayao/LineApplication.java
@@ -1,20 +1,28 @@
 package com.iphayao;
 
+import java.io.IOException;
+
+import org.apache.xmlbeans.impl.xb.ltgfmt.TestCase.Files;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import io.swagger.models.Path;
 
-@SpringBootApplication
-public class LineApplication {
+@SpringBootApplication(scanBasePackages = { "com.iphayao.linebot"
+		,"com.iphayao.repository"
+		,"com.iphayao.service"
+		,"com.iphayao.linenotify"})
+public class LineApplication extends SpringBootServletInitializer {
+	public static Path downloadedContentDir;
+    public static void main(String[] args) throws IOException {
+//        downloadedContentDir = Files.createTempDirectory("line-bot");
+        SpringApplication.run(LineApplication.class, args);
+    }
 
-  public static Path downloadedContentDir;
-	
-	public static void main(String[] args) throws IOException {
-	downloadedContentDir = Files.createTempDirectory("line-bot");
-	SpringApplication.run(LineApplication.class, args);
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LineApplication.class);
+    }
 }
--- src/main/java/com/iphayao/linebot/Application.java
+++ src/main/java/com/iphayao/linebot/Application.java
@@ -1,27 +1,27 @@
-package com.iphayao.linebot;
-
-import java.io.IOException;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import io.swagger.models.Path;
-
-@SpringBootApplication(scanBasePackages = { "com.iphayao.linebot"
-		,"com.iphayao.repository"
-		,"com.iphayao.service"
-		,"com.iphayao.linenotify"})
-public class Application extends SpringBootServletInitializer {
-	public static Path downloadedContentDir;
-    public static void main(String[] args) throws IOException {
-//        downloadedContentDir = Files.createTempDirectory("line-bot");
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-}
+//package com.iphayao.linebot;
+//
+//import java.io.IOException;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//import io.swagger.models.Path;
+//
+//@SpringBootApplication(scanBasePackages = { "com.iphayao.linebot"
+//		,"com.iphayao.repository"
+//		,"com.iphayao.service"
+//		,"com.iphayao.linenotify"})
+//public class Application extends SpringBootServletInitializer {
+//	public static Path downloadedContentDir;
+//    public static void main(String[] args) throws IOException {
+////        downloadedContentDir = Files.createTempDirectory("line-bot");
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
+//}
