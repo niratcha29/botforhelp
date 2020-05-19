bf85143b82027c950a5780a592cbe6d67efeaa4a
panchanok <panchanokkan@outlook.com>
2020-05-19 20:18:00 +0700

    im5

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -158,7 +158,8 @@ public class LineBotController {
 				this.reply(replyToken,
 						Arrays.asList(new TextMessage("1. ตรวจสอบว่าปลั๊กไฟถูกเสียบอยู่หรือไม่"+ "\n" + "2. ตรวจสอบว่าเสียบปลั๊กไฟแน่นหรือไม่\n" + 
 								"3. ตรวจสอบสวิตซ์ไฟว่าเปิดหรือไม่"),
-								new ImageMessage("https://www.picz.in.th/image/drone-1245980-640.qYZElD.jpg", "https://www.picz.in.th/image/elephant-1822636.qYV4Pt.jpg")));
+								new ImageMessage("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_5_u7GBIdoAjprniKr6n0sGl7FIMWvJjqUI_MNkFUffj8dDHG&usqp=CAU&fbclid=IwAR0l8zLHO1XB1uzg3ReeEuB01OyN8j4R3WmipebXdycaDl1tRU-lNoY1BHw", 
+										"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_5_u7GBIdoAjprniKr6n0sGl7FIMWvJjqUI_MNkFUffj8dDHG&usqp=CAU&fbclid=IwAR0l8zLHO1XB1uzg3ReeEuB01OyN8j4R3WmipebXdycaDl1tRU-lNoY1BHw.jpg")));
 				userLog.setStatusBot(status.DEFAULT);
 				break;
 			}
